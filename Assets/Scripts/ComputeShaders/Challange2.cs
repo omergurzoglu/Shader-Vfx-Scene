ac9a8ec963af8846979ef10b9127a11db9a7e359
Ömer Gürzoğlu <omergurzoglu@gmail.com>
2023-12-15 12:43:47 +0300

    continue

---
+++ Assets/Scripts/ComputeShaders/Challange2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace ComputeShaders
+{
+    public class Challenge2 : MonoBehaviour
+    {
+
+        public ComputeShader shader;
+        public int texResolution = 1024;
+
+        Renderer rend;
+        RenderTexture outputTexture;
+
+        private int sides = 6;
+        int kernelHandle;
+
+        public Color fillColor = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+        public Color clearColor = new Color( 0, 0, 0.3f, 1.0f );
+
+        // Use this for initialization
+        void Start()
+        {
+            outputTexture = new RenderTexture(texResolution, texResolution, 0);
+            outputTexture.enableRandomWrite = true;
+            outputTexture.Create();
+
+            rend = GetComponent<Renderer>();
+            rend.enabled = true;
+
+            InitShader();
+        }
+
+        private void InitShader()
+        {
+            kernelHandle = shader.FindKernel("CSMain");
+
+            shader.SetVector("fillColor", fillColor);
+            shader.SetVector("clearColor", clearColor);
+            shader.SetInt("sides",sides);
+
+            shader.SetInt("texResolution", texResolution);
+            shader.SetTexture(kernelHandle, "Result", outputTexture);
+       
+            rend.material.SetTexture("_BaseMap", outputTexture);
+        }
+
+        private void DispatchShader(int x, int y)
+        {
+            //shader.SetFloat( "time", Time.time );
+            shader.Dispatch(kernelHandle, x, y, 1);
+        }
+
+        void Update()
+        {
+            shader.SetFloat( "time", Time.time );
+            DispatchShader(texResolution / 8, texResolution / 8);
+        }
+    }
+}
--- Assets/Scripts/ComputeShaders/MeshDeform.cs
+++ Assets/Scripts/ComputeShaders/MeshDeform.cs
@@ -10,6 +10,26 @@ namespace ComputeShaders
 	
         int kernelHandle;
         Mesh mesh;
+
+        private Vertex[] vertexArray;
+        private Vertex[] initialArray; //model vertex
+        private ComputeBuffer vertexBuffer;
+        private ComputeBuffer initialBuffer;
+        public struct Vertex
+        {
+            public Vector3 position;
+            public Vector3 normal;
+
+            public Vertex(Vector3 p , Vector3 n)
+            {
+                position.x = p.x;
+                position.y = p.y;
+                position.z = p.z;
+                normal.x = n.x;
+                normal.y = n.y;
+                normal.z = n.z;
+            }
+        }
     
         // Use this for initialization
         void Start()
@@ -49,34 +69,66 @@ namespace ComputeShaders
     
         private void InitVertexArrays(Mesh mesh)
         {
-        
+            vertexArray = new Vertex[mesh.vertices.Length];
+            initialArray = new Vertex[mesh.vertices.Length];
+
+            for (int i = 0; i < vertexArray.Length; i++)
+            {
+                Vertex v1 = new Vertex(mesh.vertices[i], mesh.normals[i]);
+                vertexArray[i] = v1;
+                Vertex v2 = new Vertex(mesh.vertices[i], mesh.normals[i]);
+                initialArray[i] = v2;
+                
+            }
+
+
         }
 
         private void InitGPUBuffers()
         {
-        
+            vertexBuffer = new ComputeBuffer(vertexArray.Length, sizeof(float) * 6);
+            vertexBuffer.SetData(vertexArray);
+            initialBuffer = new ComputeBuffer(initialArray.Length, sizeof(float) * 6);
+            initialBuffer.SetData(initialArray);
+            
+            shader.SetBuffer(kernelHandle,"vertexBuffer",vertexBuffer);
+            shader.SetBuffer(kernelHandle,"initialBuffer",initialBuffer);
+                    
         }
     
-        void GetVerticesFromGPU()
+        private void GetVerticesFromGPU()
         {
-        
-        }
+            vertexBuffer.GetData(vertexArray);
+            Vector3[] vertices = new Vector3[vertexArray.Length];
+            Vector3[] normals = new Vector3[vertexArray.Length];
+            for (int i = 0; i < vertexArray.Length; i++)
+            {
+                vertices[i] = vertexArray[i].position;
+                normals[i] = vertexArray[i].normal;
+            }
 
-        void Update(){
+            mesh.vertices = vertices;
+            mesh.normals = normals;
+
+        }
+        
+ 
+        private void Update(){
             if (shader)
             {
                 shader.SetFloat("radius", radius);
                 float delta = (Mathf.Sin(Time.time) + 1)/ 2;
                 shader.SetFloat("delta", delta);
-                shader.Dispatch(kernelHandle, 1, 1, 1);
+                shader.Dispatch(kernelHandle, vertexArray.Length, 1, 1);
             
                 GetVerticesFromGPU();
             }
         }
 
-        void OnDestroy()
+        private void OnDestroy()
         {
-        
+            vertexBuffer.Dispose();
+            initialBuffer.Dispose();
         }
     }
 }
