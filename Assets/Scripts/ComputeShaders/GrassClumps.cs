aa831786d43e1a7dd0802d53daa9123122c24caa
Ömer Gürzoğlu <omergurzoglu@gmail.com>
2023-12-23 16:06:20 +0300

    grass cs todo : change surface to vert frag

---
+++ Assets/Scripts/ComputeShaders/GrassClumps.cs
@@ -0,0 +1,102 @@
+﻿
+using UnityEngine;
+
+public class GrassClumps : MonoBehaviour
+{
+    struct GrassClump
+    {
+        public Vector3 position;
+        public float lean;
+        public float noise;
+
+        public GrassClump( Vector3 pos)
+        {
+            position.x = pos.x;
+            position.y = pos.y;
+            position.z = pos.z;
+            lean = 0;
+            noise = Random.Range(0.5f, 1);
+            if (Random.value < 0.5f) noise = -noise;
+        }
+    }
+    int SIZE_GRASS_CLUMP = 5 * sizeof(float);
+
+    public Mesh mesh;
+    public Material material;
+    public ComputeShader shader;
+    [Range(0,1)]
+    public float density = 0.8f;
+    [Range(0.1f,3)]
+    public float scale = 0.2f;
+    [Range(10, 45)]
+    public float maxLean = 25;
+
+    ComputeBuffer clumpsBuffer;
+    ComputeBuffer argsBuffer;
+    GrassClump[] clumpsArray;
+    uint[] argsArray = new uint[] { 0, 0, 0, 0, 0 };
+    Bounds bounds;
+    int timeID;
+
+    int groupSize;
+
+    int kernelLeanGrass;
+
+    void InitShader()
+    {
+        MeshFilter ms = GetComponent<MeshFilter>();
+        Bounds bounds = ms.sharedMesh.bounds;
+        Vector3 clumps = bounds.extents;
+        Vector3 vec = transform.localScale / 0.1f * density;
+        clumps.x *= vec.x;
+        clumps.z *= vec.z;
+        int total = (int)(clumps.x * clumps.z);
+
+        kernelLeanGrass = shader.FindKernel("LeanGrass");
+        uint threadGroupSize;
+        shader.GetKernelThreadGroupSizes(kernelLeanGrass, out threadGroupSize, out _, out _);
+        groupSize = Mathf.CeilToInt((float)total / (float)threadGroupSize);
+        int count = groupSize * (int)threadGroupSize;
+        clumpsArray = new GrassClump[count];
+
+
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 pos = new Vector3(Random.value*bounds.extents.x*2-bounds.extents.x +bounds.center.x, 0, Random.value * bounds.extents.z * 2 - bounds.extents.z);
+            pos = transform.TransformPoint(pos);
+            clumpsArray[i] = new GrassClump(pos);
+        }
+        clumpsBuffer = new ComputeBuffer(count, SIZE_GRASS_CLUMP);
+        clumpsBuffer.SetData(clumpsArray);
+        shader.SetBuffer(kernelLeanGrass, "clumps", clumpsBuffer);
+        shader.SetFloat("maxLean",maxLean*Mathf.PI /180);
+        timeID = Shader.PropertyToID("time");
+        material.SetBuffer("clumpsBuffer",clumpsBuffer);
+        material.SetFloat("_Scale",scale);
+        argsArray[0] = mesh.GetIndexCount(0);
+        argsArray[1] = (uint)count;
+        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(argsArray);
+    }
+
+    void Start()
+    {
+        bounds = new Bounds(Vector3.zero, new Vector3(30, 30, 30));
+        InitShader();
+    }
+
+    void Update()
+    {
+        shader.SetFloat(timeID, Time.time);
+        shader.Dispatch(kernelLeanGrass, groupSize, 1, 1);
+
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
+    }
+
+    private void OnDestroy()
+    {
+        if (clumpsBuffer != null) clumpsBuffer.Release();
+        if (argsBuffer != null) argsBuffer.Release();
+    }
+}
---
+++ Assets/Scripts/ComputeShaders/GrassClumps.shader
@@ -0,0 +1,101 @@
+﻿//Shader "Custom/GrassClumps"
+//{
+//    Properties
+//    {
+//        _Color ("Color", Color) = (1,1,1,1)
+//        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+//        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+//        _Metallic ("Metallic", Range(0,1)) = 0.0
+//    }
+//    SubShader
+//    {
+//        Tags{ "RenderType"="Opaque" }
+//        
+//		LOD 200
+//		Cull Off
+//		
+//        CGPROGRAM
+//      
+//        #pragma vert vertex
+//        #pragma frag fragment
+//        #pragma instancing_options procedural:setup
+//        sampler2D _MainTex;
+//        struct Input
+//        {
+//            float2 uv_MainTex;
+//        };
+//        half _Glossiness;
+//        half _Metallic;
+//        fixed4 _Color;
+//        float _Scale;
+//        float4x4 _Matrix;
+//        float3 _Position;
+//
+//        float4x4 create_matrix(float3 pos, float theta){
+//            float c = cos(theta);
+//            float s = sin(theta);
+//            return float4x4(
+//                c,-s, 0, pos.x,
+//                s, c, 0, pos.y,
+//                0, 0, 1, pos.z,
+//                0, 0, 0, 1
+//            );
+//        }
+//
+//        struct v2f {
+//            float4 vertex : SV_POSITION;
+//            float3 normal : NORMAL;
+//            float2 texcoord : TEXCOORD0;
+//        }; 
+//        
+//        struct appdata {
+//            float4 vertex : POSITION;
+//            float3 normal : NORMAL;
+//            float2 texcoord : TEXCOORD0;
+//            UNITY_VERTEX_INPUT_INSTANCE_ID
+//        }; 
+//       
+//        
+//            struct GrassClump
+//            {
+//                float3 position;
+//                float lean;
+//                float noise;
+//            };
+//            StructuredBuffer<GrassClump> clumpsBuffer; 
+//
+//        void vert(inout appdata_full v, out Input data)
+//        {
+//            UNITY_INITIALIZE_OUTPUT(Input, data);
+//            
+//                v.vertex.xyz *= _Scale;
+//                float4 rotatedVertex = mul(_Matrix,v.vertex);
+//                v.vertex.xyz += _Position ;
+//                v.vertex= lerp(v.vertex,rotatedVertex,v.texcoord.y);
+//          
+//        }
+//
+//        void setup()
+//        {
+//           
+//                GrassClump clump = clumpsBuffer[unity_InstanceID];
+//                _Position = clump.position;
+//                _Matrix = create_matrix(clump.position, clump.lean);
+//           
+//        }
+//
+//        // void surf (Input IN, inout SurfaceOutputStandard o)
+//        // {
+//        //     // Albedo comes from a texture tinted by color
+//        //     fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+//        //     o.Albedo = c.rgb;
+//        //     // Metallic and smoothness come from slider variables
+//        //     o.Metallic = _Metallic;
+//        //     o.Smoothness = _Glossiness;
+//        //     o.Alpha = c.a;
+//        //     clip(c.a-0.4);
+//        // }
+//        ENDCG
+//    }
+//    FallBack "Diffuse"
+//}
